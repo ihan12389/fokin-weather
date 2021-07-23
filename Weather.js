2fd44fb57ad12f72a77eeb3e176f966d0ab1f21b
ihan12389 <ihan1289@gmail.com>
2021-07-24 00:34:43 +0900

    final update

--- Weather.js
+++ Weather.js
@@ -1,12 +1,93 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 import PropTypes from "prop-types";
+import { LinearGradient } from "expo-linear-gradient";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function Weatehr({ temp }) {
+const weatherOptions = {
+  Thunderstorm: {
+    iconName: "weather-lightning",
+    gradient: ["#373B44", "#4286f4"],
+    title: "Thunderstorm",
+    subtitle: "밖인데 배터리가 1퍼라면 핸드폰을 높이 들고 기적을 기다려보세요",
+  },
+  Drizzle: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"],
+    title: "이슬비",
+    subtitle: "남자라면 이 정도 비에 우산을 쓰지 않습니다.",
+  },
+  Rain: {
+    iconName: "weather-rainy",
+    gradient: ["#00C6FB", "#005BEA"],
+    title: "비",
+    subtitle: "비 때문에 밖에 안나가는 거라고 합리화하기 좋은 날",
+  },
+  Snow: {
+    iconName: "weather-snowy",
+    gradient: ["#7DE2FC", "#B9B6E5"],
+    title: "눈",
+    subtitle: "누나, 눈 와!",
+  },
+  Atmosphere: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"],
+  },
+  Clear: {
+    iconName: "weather-sunny",
+    gradient: ["#FF7300", "#FEF253"],
+    title: "맑음",
+    subtitle: "이렇게 날이 좋은데 집에만 있으려구요?",
+  },
+  Clouds: {
+    iconName: "weather-cloudy",
+    gradient: ["#D7D2CC", "#304352"],
+    title: "구름",
+    subtitle: "저 구름 위엔 라퓨타가 있을거야",
+  },
+  Haze: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+    title: "Haze",
+    subtitle: "Just don't go outside.",
+  },
+  Mist: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+    title: "Mist",
+    subtitle: "Just don't go outside",
+  },
+  Dust: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+    title: "Dust",
+    subtitle: "콜록 콜록 켁켁 컥!",
+  },
+};
+
+export default function Weatehr({ temp, condition }) {
+  console.log(condition);
   return (
-    <View style={styles.container}>
-      <Text>{temp}</Text>
-    </View>
+    <LinearGradient
+      colors={weatherOptions[condition].gradient}
+      style={styles.container}
+    >
+      <StatusBar barStyle={"light-content"} />
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons
+          size={96}
+          name={weatherOptions[condition].iconName}
+          color="white"
+        />
+        <Text style={styles.temp}>{temp}℃</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherOptions[condition].subtitle}
+        </Text>
+      </View>
+    </LinearGradient>
   );
 }
 
@@ -29,7 +110,33 @@ Weatehr.propTypes = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  temp: {
+    fontSize: 42,
+    color: "white",
+  },
+  halfContainer: {
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  title: {
+    color: "white",
+    fontSize: 44,
+    fontWeight: "300",
+    marginBottom: 10,
+    textAlign: "left",
+  },
+  subtitle: {
+    fontWeight: "600",
+    color: "white",
+    fontSize: 24,
+    textAlign: "left",
+  },
+  textContainer: {
+    paddingHorizontal: 40,
+    alignItems: "flex-start",
+    justifyContent: "center",
+    flex: 1,
   },
 });
